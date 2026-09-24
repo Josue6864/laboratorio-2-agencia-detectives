498df1fc2eec846ec69f635a6fc1cbc23e8b8dcb
Josue Garcia <gar26375@uvg.edu.gt>
2026-09-24 09:32:55 -0600

    add: se agregaron archivos de modelo

---
+++ src/main/java/com/mycompany/agenciadetectives/AgenciaDetectives.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agenciadetectives;
+
+/**
+ *
+ * @author david_oavv7c4
+ */
+public class AgenciaDetectives {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
